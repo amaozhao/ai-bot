b6f5802956d1a44a6eaba030ae20398e6f405683
amaozhao <amaozhao@gmail.com>
2023-04-14 18:02:19 +0800

    [WIP]

--- bot/chat/routers/auth.py
+++ bot/chat/routers/auth.py
@@ -30,7 +30,18 @@ async def signin(sigin_schema: SignIn):
 
 @router.post("/signup")
 async def signup(signup_schema: SignUp):
-    user = await user_service.create(
+    user = await user_service.get(
+        username=signup_schema.username,
+        email=signup_schema.email,
+        mobile=signup_schema.mobile,
+    )
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username or email or mobile",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    await user_service.create(
         username=signup_schema.username,
         mobile=signup_schema.mobile,
         password=signup_schema.password1,
--- bot/chat/services/user.py
+++ bot/chat/services/user.py
@@ -1,13 +1,9 @@
-from datetime import datetime, timedelta, timezone
-from typing import Union
-
-import jwt
+from fastapi import HTTPException, status
 from passlib.context import CryptContext
-from sqlalchemy import select
+from sqlalchemy import or_, select
 from sqlalchemy.orm import selectinload
 
 from ..models import User
-from ..settings import settings
 from .base import BaseService
 
 
@@ -26,19 +22,63 @@ class UserService(BaseService):
             )
             session.add(user)
             await session.commit()
-            print(self.pwd_context.hash(password), 1111)
             return user
 
-    async def get(self, username):
+    async def check(self, username, email, mobile):
         async with self.session.begin() as session:
             stmt = (
                 select(User)
-                .where(User.username == username)
+                .filter(
+                    or_(
+                        User.username == username,
+                        User.email == email,
+                        User.mobile == mobile,
+                    )
+                )
                 .options(selectinload(User.profile))
                 .limit(1)
             )
             result = await session.execute(stmt)
             return result.scalars().first()
+
+    async def get(self, username=None, email=None, mobile=None):
+        if not (username or email or mobile):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        async with self.session.begin() as session:
+            stmt = (
+                select(User)
+                .filter(
+                    or_(
+                        User.username == username,
+                        User.email == email,
+                        User.mobile == mobile,
+                    )
+                )
+                .options(selectinload(User.profile))
+                .limit(1)
+            )
+            result = await session.execute(stmt)
+            return result.scalars().first()
+
+    async def update_password(self, username, password: str):
+        user = await self.get(username=username)
+        async with self.session.begin() as session:
+            stmt = (
+                select(User)
+                .filter(User.username == username)
+                .options(selectinload(User.profile))
+                .limit(1)
+            )
+            result = await session.execute(stmt)
+            if user := result.scalars().first():
+                user.password = self.pwd_context.hash(password)
+                await session.commit()
+                return user
+            return None
 
 
 user_service = UserService()
